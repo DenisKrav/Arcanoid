9ccc2b53eaa11053876c03d6c792d644713532e0
DenisKrav <den.325.2002.5@gmail.com>
2024-04-13 21:29:55 +0300

    Initial commit

--- main.py
+++ main.py
@@ -22,9 +22,31 @@ class Area():
     def fill(self):
         pygame.draw.rect(screen, self.fill_color, self.rect)
 
+class Picture(Area):
+    def __init__(self, fileName, x=0, y=0, width=10, height=10, color=(156, 37, 134)):
+        Area.__init__(self, x, y, width, height, color)
+        self.image = pygame.transform.scale(pygame.image.load(fileName), (width, height))
 
+    def draw_picture(self):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+class Label(Area):
+    def __init__(self, x=0, y=0, width=10, height=10, color=None):
+        super().__init__(x, y, width, height, color)
+
+    def set_text(self, text, fsize=12, text_color=(0, 0, 0)):
+        self.text = text
+        self.image = pygame.font.SysFont('verdana', fsize).render(text, True, text_color)
+
+    def draw_text(self, shift_x=0, shift_y=0):
+        self.fill()
+        screen.blit(self.image, (self.rect.x + shift_x, self.rect.y + shift_y))
+
+platform = Picture("images/platform.png", int(screenWidth * 0.35), int(screenHeight * 0.8), 200, 50, (209, 161, 209))
 
 while True:
+    platform.draw_picture()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
